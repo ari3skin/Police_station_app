62ea733c137053a1f98b1318a949714fbc298510
Ryan <ryankinotikathurima@gmail.com>
2022-06-29 15:14:33 +0300

    creating the repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module application.police_station_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens application.police_station_app to javafx.fxml;
+    exports application.police_station_app;
+}
