eebdcd0936768fdd9d16d9a576bb2707f4eac177
Ryan <ryankinotikathurima@gmail.com>
2022-07-02 16:32:12 +0300

    welcome-login-register-pages-done

--- src/main/java/application/police_station_app/RegisterController.java
+++ src/main/java/application/police_station_app/RegisterController.java
@@ -1,4 +1,22 @@
 package application.police_station_app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class RegisterController {
+    Scene scn1;
+    FXMLLoader loader;
+    Stage stage;
+
+    public void back(ActionEvent e) throws IOException {
+        loader = new FXMLLoader(WelcomePageController.class.getResource("WelcomePage.fxml"));
+        scn1 = new Scene(loader.load());
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scn1);
+        stage.show();
+    }
 }
--- src/main/java/application/police_station_app/WelcomePage.java
+++ src/main/java/application/police_station_app/WelcomePage.java
@@ -1,50 +1,63 @@
 package application.police_station_app;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.File;
+import java.util.Objects;
 
 public class WelcomePage extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
     @Override
     public void start(Stage stage) throws Exception {
-        Image icon = new Image(getClass().getResourceAsStream("logo1.png"));
+        //adding the application icon
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("logo1.png")));
         stage.getIcons().add(icon);
+
+        //loading the fxml file
         FXMLLoader loader = new FXMLLoader(WelcomePage.class.getResource("WelcomePage.fxml"));
         Scene scn1 = new Scene(loader.load());
 
+        //initiating stage settings
         stage.setTitle("Kenya Police Service Program");
+        stage.setResizable(false);
         stage.setScene(scn1);
         stage.show();
 
+        //below ensures the x close button works
         stage.setOnCloseRequest(e -> {
             e.consume();
             exit(stage);
         });
     }
 
-    public void exit(Stage stage){
+    public void exit(Stage stage) {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Exit Program");
         alert.setHeaderText("You are about to Exit");
         alert.setContentText("Press Cancel to go back");
 
-        if (alert.showAndWait().get() == ButtonType.OK){
+        if (alert.showAndWait().get() == ButtonType.OK) {
             System.out.println("You have exited the program");
             stage.close();
         }
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
 
 }
