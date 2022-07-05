9cd60af62b6c2fb4cf702adeaabe02f397b2bc16
edwin <eddiendiritu@gmail.com>
2022-07-05 21:43:09 +0300

    Report User Interface

---
+++ src/main/java/application/police_station_app/Dashboard.java
@@ -0,0 +1,24 @@
+package application.police_station_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Dashboard extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Dashboard.class.getResource("dashboard.fxml"));
+        primaryStage.setTitle("Police Dashboard");
+        primaryStage.setScene(new Scene(root,1000,780));
+        primaryStage.show();
+    }
+}
